633a5516483e862b78b197038f80542d7003735d
Mina Saad <minasaad565@gmail.com>
2026-03-11 19:01:49 +0100

    first commit

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'tsc-v1';
+const ASSETS = ['/'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Don't cache cross-origin requests (Firebase, iframe)
+  if (!e.request.url.startsWith(self.location.origin)) return;
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
